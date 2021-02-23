9d77f78dd894b143f6a7287a7f645c6cb3a08641
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-02-23 10:45:54 +1000

    update profiles to match ProfCat defaults

--- model/profiles.py
+++ model/profiles.py
@@ -38,23 +38,12 @@ profile_sdo = Profile(
     default_language="en",
 )
 
-# profile_skos = Profile(
-#     "https://www.w3.org/TR/skos-reference/",
-#     label="SKOS",
-#     comment="Simple Knowledge Organization System (SKOS)is a W3C-authored, common data model for sharing "
-#     "and linking knowledge organization systems "
-#     "via the Web.",
-#     mediatypes=["text/html", "application/json"] + Renderer.RDF_MEDIA_TYPES,
-#     default_mediatype="text/html",
-#     languages=["en"],  # default 'en' only for now
-#     default_language="en",
-# )
-
 profile_slist = Profile(
     "https://w3id.org/profile/slist",
     label="Catalogue Simple List",
-    comment="A simple JSON object containing basic catalogue mtadata and a list of the Resources it contains, "
-            "possibly pre-filtered.",
+    comment="A simple JSON object containing basic catalogue metadata and a list of the Resources it contains. "
+            "A filterClass query string argument indicating an RDFS/OWL class may be used to filter catalogue members "
+            "by type",
     mediatypes=["application/json"],
     default_mediatype="application/json",
     languages=["en"],  # default 'en' only for now
